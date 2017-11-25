a20bdf77d4dd618493b6d3fcab962cd472817c01
holdonnn <rururu0729@gmail.com>
2017-11-26 03:10:23 +0900

    Header.js

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,28 +3,44 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 
-import './Header.css'
-
 const HeaderWrapper = styled.div`
-  margin-bottom: 1.45rem;
-  padding: 1.45rem 1rem;
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   opacity: 0.9;
   display: flex;
+  background: red;
+  height: ${props => props.expand ? 200 : 50}px;
+  transition: height 0.5s;
 `
 
 const HeaderTitle = styled(Link)`
   text-decoration: none;
-    color: inherit;
+  color: inherit;
+`
+const HeaderSearchIcon = styled.a`
+
 `
 
-const Header = () => (
-  <HeaderWrapper className='Header'>
-    <HeaderTitle to="/">Holdonnn!!!!</HeaderTitle>
-  </HeaderWrapper>
-);
+class Header extends React.Component {
+
+  state = {
+    searchMode: true
+  }
+
+  onClickSearchIcon = () => {
+    this.setState({searchMode: !this.state.searchMode})
+  }
+
+  render() {
+    return (
+      <HeaderWrapper expand={this.state.searchMode}>
+        <HeaderTitle to="/">Holdonnn!!!!</HeaderTitle>
+        <HeaderSearchIcon onClick={this.onClickSearchIcon}>Search</HeaderSearchIcon>
+      </HeaderWrapper>
+    )
+  }
+}
 
 export default Header;
