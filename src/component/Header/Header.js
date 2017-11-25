5bcc558a90044e8846adc064138789f2f6ea9352
holdonnn <rururu0729@gmail.com>
2017-11-26 02:13:41 +0900

    Header styled component

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-import './Header.css';
+import './Header.css'
+
+const HeaderWrapper = styled.div`
+  margin-bottom: 1.45rem;
+  padding: 1.45rem 1rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.9;
+  display: flex;
+`
+
+const HeaderTitle = styled(Link)`
+  text-decoration: none;
+    color: inherit;
+`
 
 const Header = () => (
-  <div className="Header">
-    <div className="Header__container">
-      <h1 className="Header__title">
-        <Link to="/" className="Header__title-link">Holdonnn</Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderWrapper className='Header'>
+    <HeaderTitle to="/">Holdonnn</HeaderTitle>
+  </HeaderWrapper>
 );
 
 export default Header;
