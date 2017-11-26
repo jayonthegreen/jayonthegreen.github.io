6794eefb5d2e80a2b430bd5b45fa2f49d3303644
holdonnn <rururu0729@gmail.com>
2017-11-26 19:57:27 +0900

    visitor first

--- src/pages/visitor.js
+++ src/pages/visitor.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgFace from './visitor/esens_face.png'
+import ImgEyeLeft from './visitor/esens_left.png'
+import ImgEyeRight from './visitor/esens_right.png'
 
 const Wrapper = styled.div`
    background-color: green;
    display: flex;
    flex-direction: column;
    height: 100vh;
+   align-items: center;
 `
+
+const FaceWrapper = styled.div`
+   position: relative;
+   background-color: red;
+   width: 100%;
+   height: 100%;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+`
+
+const Face = styled.div`
+   width: 420px;
+   height: 500px;
+   background-image: url('${props => props.backgroundImage}');
+   z-index: 11;
+`
+
+const Eyes = styled.div`
+  background-color: green;
+  width: 100%;
+  position: absolute;
+  top: calc(50% - 85px);
+  text-align: center;
+  z-index: 10;
+  transform: translate(${props => props.x}px);
+  transition: all 1s;
+`
+
+const EyeLeft = styled.img`
+  // margin-left: -10px; 
+  margin-bottom: 0;
+  padding-right: 12px;
+  margin-left: -8px;
+`
+
+const EyeRight = styled.img`
+  margin-bottom: 0;
+`
+
 
 const WiperWrapper = styled.div`
    margin-top: auto;
@@ -27,21 +71,30 @@ const SwipeBall = styled.div`
 
 class VisitorPage extends React.Component {
 
-  componentDidMount() {
-
+  state = {
+    swing: true
   }
 
+  componentDidMount() {
+    setInterval(() => this.setState({swing: !this.state.swing}), 1000 )
+  }
 
-  render () {
+  render() {
     return <Wrapper>
-      VisitorPage
+      <FaceWrapper>
+        <Face backgroundImage={ImgFace}/>
+        <Eyes x={this.state.swing ? 8 : -8}>
+          <EyeLeft src={ImgEyeLeft}/>
+          <EyeRight src={ImgEyeRight}/>
+        </Eyes>
+      </FaceWrapper>
       <WiperWrapper>
         <SwipeBall/>
       </WiperWrapper>
-      <audio autoPlay loop>
-        <source src="https://s3.ap-northeast-2.amazonaws.com/static.holdonnn.me/mp3/e-sens-visitor.mp3" type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      {/*<audio autoPlay loop>*/}
+      {/*<source src="https://s3.ap-northeast-2.amazonaws.com/static.holdonnn.me/mp3/e-sens-visitor.mp3" type="audio/mpeg" />*/}
+      {/*Your browser does not support the audio element.*/}
+      {/*</audio>*/}
     </Wrapper>
   }
 }
