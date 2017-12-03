2fcee62a8a50d750dae6f7c1891786d516e1001d
holdonnn <rururu0729@gmail.com>
2017-12-03 19:56:49 +0900

    fix GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,5 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-77395473-1');
 
 exports.onRouteUpdate = (state, page, pages) => {
-  ReactGA.pageview(state.pathname);
+  ReactGA.pageview(state.location.pathname);
 };
