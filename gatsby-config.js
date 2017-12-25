ada6d0373bdc0e25501c4d940ba807227f12ba1f
holdonnn <rururu0729@gmail.com>
2017-12-25 19:59:22 +0900

    add 'gatsby-plugin-styled-components'

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,    
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-styled-components`,
   ],
 }
