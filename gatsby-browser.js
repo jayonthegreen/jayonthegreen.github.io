daac017eeacb4a0ea98888793069b8e45b395edc
holdonnn <rururu0729@gmail.com>
2018-04-08 14:30:16 +0900

    update trim code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,18 +2,10 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-77395473-1');
 
 exports.onRouteUpdate = (state, page, pages) => {
-  try{
-    const query  = state.location.search.substring(1);
-    if ( query.includes('isAdmin') ) {
-      window.localStorage.setItem('isAmdin', 'true');
-    }
-  } catch (e){
-    throw new Error(e);
+  if ( state.location.search.includes('isAdmin') ) {
+    window.localStorage.setItem('isAmdin', 'true');
   }
   if (process.env.NODE_ENV === 'production' && !window.localStorage.getItem('isAmdin')) {
     ReactGA.pageview(state.location.pathname);
-    console.log('[ok]ReactGA.pageview');
-  } else {
-    console.log('[not ok]ReactGA.pageview ');
-  }
+  } 
 };
