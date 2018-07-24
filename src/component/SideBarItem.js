4a2ca1010556e62e8f4739d58b39fe2fb8470946
holdonnn <rururu0729@gmail.com>
2018-07-24 17:33:51 +0900

    fix

--- src/component/SideBarItem.js
+++ src/component/SideBarItem.js
@@ -2,16 +2,24 @@ import React from 'react'
 import NavLink from 'gatsby-link'
 import styled from 'styled-components'
 
+const Wrapper = styled.div`
+    a{
+      font-size: ${props => props.isSub ? '0.7' :'0.8'}rem;
+      padding-left: ${props => props.isSub ? 60 : 50}px;
+      margin-top: ${props => props.isSub ? 10 : 30}px;
+    }
+  
+`
+
 const Item = styled(NavLink)`
     text-decoration: none;
     display: block;
     font-size: ${props => props.isSub ? '0.7' :'0.8'}rem;
     padding-left: ${props => props.isSub ? 60 : 50}px;
     margin-top: ${props => props.isSub ? 10 : 30}px;
-  
-      color: inherit;
-      text-decoration: none;
-      &.active{font-weight: bold;}
+    color: inherit;
+    text-decoration: none;
+    &.active{font-weight: bold;}
   
 `
 
@@ -19,13 +27,12 @@ class SideBarItem extends React.Component {
   
   render() {
     return (
+      <Wrapper isSub={this.props.isSub}>
           <Item
             exact to={this.props.to}
             onClick={this.props.onClick}
-            isSub={this.props.isSub}
-            style={{color: 'inherit'}}
             exact to={this.props.to}
-            onClick={this.props.onClick}>{this.props.text}</Item>
+            onClick={this.props.onClick}>{this.props.text}</Item></Wrapper>
     )
   }
 }
