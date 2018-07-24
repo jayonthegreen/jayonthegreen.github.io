b2e9c64a17bb1056a763430a33a13b7953699c09
holdonnn <rururu0729@gmail.com>
2018-07-24 17:07:26 +0900

    사이드 네비게이션 클릭영역 확대

--- src/component/SideBarItem.js
+++ src/component/SideBarItem.js
@@ -2,36 +2,30 @@ import React from 'react'
 import NavLink from 'gatsby-link'
 import styled from 'styled-components'
 
-const Wrapper = styled.div`
+const Item = styled(NavLink)`
     text-decoration: none;
     display: block;
     font-size: ${props => props.isSub ? '0.7' :'0.8'}rem;
     padding-left: ${props => props.isSub ? 60 : 50}px;
     margin-top: ${props => props.isSub ? 10 : 30}px;
-    
-    a {
+  
       color: inherit;
       text-decoration: none;
-      &.active{
-      font-weight: bold;
-    }
-    }
+      &.active{font-weight: bold;}
+  
 `
 
 class SideBarItem extends React.Component {
   
   render() {
     return (
-      <Wrapper
-          exact to={this.props.to}
-          onClick={this.props.onClick}
-          isSub={this.props.isSub}
-      >
-          <NavLink
+          <Item
+            exact to={this.props.to}
+            onClick={this.props.onClick}
+            isSub={this.props.isSub}
             style={{color: 'inherit'}}
             exact to={this.props.to}
-            onClick={this.props.onClick}>{this.props.text}</NavLink>
-      </Wrapper>
+            onClick={this.props.onClick}>{this.props.text}</Item>
     )
   }
 }
