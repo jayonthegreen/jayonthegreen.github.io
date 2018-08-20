5904f27acc4c6e33c62543f606aa4aaea762b29c
holdonnn <rururu0729@gmail.com>
2018-08-21 02:08:13 +0900

    update version

--- src/html.js
+++ src/html.js
@@ -24,7 +24,7 @@ export default class HTML extends React.Component {
     return (
       <html lang="ko">
         <head>
-          <meta version="0.1.1"/>
+          <meta version="0.1.2"/>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           {this.props.headComponents}
