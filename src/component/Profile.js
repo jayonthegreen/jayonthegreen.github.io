b444cc5391733a080e6562be379a5f0a3ab76926
holdonnn <rururu0729@gmail.com>
2018-09-26 02:37:14 +0900

    Add box shadow

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -12,10 +12,35 @@ const Wrapper = styled.div`
 const Title = styled.div`
   display: inline-block;
   font-weight: bold;
-  font-size: 1.0rem;
+  font-size: 1.5rem;
   margin: 0.5rem 0;
   padding: 0.25rem 0.5rem;
   ${ hoverCss}
+  box-shadow: inset 0 -6px 0 rgba(36,177,209,.2);
+  /* animation-name: color_change;
+	animation-duration: 1s;
+	animation-iteration-count: infinite;
+	animation-direction: alternate;
+  @-webkit-keyframes color_change {
+	from { box-shadow: inset 0 -6px 0 rgba(36,177,209,.2); }
+	to { box-shadow: inset 0 -6px 0 rgba(184,107,255,.2); }
+  }
+@-moz-keyframes color_change {
+	from { box-shadow: inset 0 -6px 0 rgba(36,177,209,.2); }
+	to { box-shadow: inset 0 -6px 0 rgba(184,107,255,.2); }
+}
+@-ms-keyframes color_change {
+	from { box-shadow: inset 0 -6px 0 rgba(36,177,209,.2); }
+	to { box-shadow: inset 0 -6px 0 rgba(184,107,255,.2); }
+}
+@-o-keyframes color_change {
+	from { box-shadow: inset 0 -6px 0 rgba(36,177,209,.2); }
+	to { box-shadow: inset 0 -6px 0 rgba(184,107,255,.2); }
+}
+@keyframes color_change {
+	from { box-shadow: inset 0 -6px 0 rgba(36,177,209,.2); }
+	to { box-shadow: inset 0 -6px 0 rgba(184,107,255,.2); }
+} */
 `
 
 const Description = styled.div`
