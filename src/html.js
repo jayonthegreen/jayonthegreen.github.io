675a7a67f7e7c11fe19afd4c6c437bb8fdb8b007
byjay <rururu0729@gmail.com>
2020-10-03 13:39:15 +0900

    google search console verification

--- src/html.js
+++ src/html.js
@@ -9,6 +9,7 @@ export default class HTML extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="google-site-verification" content="JA_ocNhbTh2OZYc6XdvGU86Tu-KrppkZddT3_sfwTDU" />
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
           <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Noto+Sans+KR&display=swap" rel="stylesheet"/>
