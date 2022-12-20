6166983f1eac6725aeee0d9862694ad831563797
byjay <rururu0729@gmail.com>
2022-12-21 06:44:16 +0900

    Revert "Move files"
    This reverts commit 92f8e211c49c2f9e3bdc7914ba3aa3749072c581.

---
+++ src/pages/wiki/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import Layout from '../../component/Layout'
+import PostList from '../../component/PostList'
+
+
+export default () => (
+    <StaticQuery
+      query={graphql`
+        query WikiQuery {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { frontmatter: { templateKey: { eq: "wiki" } } }
+          ) {
+            edges {
+              node {
+                excerpt(pruneLength: 400)
+                id
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                  description
+                  templateKey
+                  date(formatString: "MMMM DD, YYYY")
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data) => (<Layout>  
+        <PostList
+          markdownNodes={data.allMarkdownRemark.edges.map(
+            ({ node }) => node
+          )}
+        />
+      </Layout>)}
+    />
+  )
