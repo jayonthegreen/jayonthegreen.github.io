59385b5e3fd7c47e064267ccf778d1dc3aa6bfa7
Jaehyun Baek <rururu0729@gmail.com>
2023-07-09 18:56:51 +0900

    Revert "Renewal (#537)"
    This reverts commit 266063dbb328f68261fd89da550f2f0b73ca0b87.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,41 @@
 import React from 'react'
-import {graphql, StaticQuery, useStaticQuery} from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../component/Layout'
 import PostList from '../component/PostList'
-import pathsToTree from "../utils/pathsToTree";
 
-
-
-export default function IndexPage() {
-    const data = useStaticQuery(query2)
-    const posts = data.allFile.edges.map(({node}) => ({
-                id: node.id,
-                relativePath: node.relativePath,
-                href: node.relativePath.split('pages')[1].replace('.md','/'),
-                title: node.name
-            }
-        )
+class IndexPage extends React.Component {
+  render() {
+    return (
+      <Layout>  
+        <PostList
+          markdownNodes={this.props.data.allMarkdownRemark.edges.map(
+            ({ node }) => node
+          )}
+        />
+      </Layout>
     )
-
-    // console.log(pathsToTree(posts.map(post => post.relativePath)))
-    return (<Layout><PostList posts={posts}/></Layout>)
+  }
 }
 
+export default IndexPage
 
-const query2 = graphql`
-query {
-  allFile(
-    filter: { 
-      sourceInstanceName: { eq: "src" },
-      extension: { eq: "md" }
-    },
-    sort:{ modifiedTime: DESC}
-  ) {
+export const query = graphql`
+query IndexQuery {
+  allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+    totalCount
     edges {
       node {
         id
-        name 
-        relativePath
-        extension
-        modifiedTime
+        frontmatter {
+          title
+          date(formatString: "YYYY.MM.DD")
+          description
+        }
+        fields {
+          slug
+        }
       }
     }
   }
 }
 `
-
-
-
