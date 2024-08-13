06d7209ee0501138dcd4874c84689829b363107d
byjay <rururu0729@gmail.com>
2024-08-13 22:55:31 +0900

    Add gatsby-plugin-git-lastmod

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,34 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage {
+            nodes {
+              path
+              pageContext
+            }
+          }
+        }
+        `,
+        serialize: ({ path, pageContext }) => {
+          return {
+            url: path,
+            lastmod: pageContext?.lastMod,
+          }
+        },
+      },
+    },
+    `gatsby-plugin-git-lastmod`,
+
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
