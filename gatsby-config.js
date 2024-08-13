6044cb5ae5103d5bff3cbf12633e05ee2e5489af
byjay <rururu0729@gmail.com>
2024-08-13 23:08:53 +0900

    Add comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  siteMetadata: {
-    title: `byjay`,
-    image: `/img/og.jpeg`,
-    siteUrl: `https://byjay.github.io`,
-  },
-  plugins: [
-    'gatsby-plugin-netlify-cms',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
+    siteMetadata: {
+        title: `byjay`,
+        image: `/img/og.jpeg`,
+        siteUrl: `https://byjay.github.io`,
     },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        query: `
+    plugins: [
+        'gatsby-plugin-netlify-cms',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-sitemap`,
+            options: {
+                query: `
         {
           site {
             siteMetadata {
@@ -34,23 +34,24 @@ module.exports = {
           }
         }
         `,
-        serialize: ({ path, pageContext }) => {
-          return {
-            url: path,
-            lastmod: pageContext?.lastMod.split('T')[0] // for YYYY-mm-dd
-            ,
-          }
+                serialize: ({path, pageContext}) => {
+                    return {
+                        url: path,
+                        // for YYYY-mm-dd, see https://developers.google.com/search/blog/2006/04/using-lastmod-attribute
+                        lastmod: pageContext?.lastMod.split('T')[0]
+                        ,
+                    }
+                },
+            },
         },
-      },
-    },
-    `gatsby-plugin-git-lastmod`,
+        `gatsby-plugin-git-lastmod`,
 
-    {
-      resolve: `gatsby-plugin-gtag`,
-      options: {
-          trackingId: `G-P76W5VY5NZ`,
-          head: true,
-      },
-    },
-  ],
+        {
+            resolve: `gatsby-plugin-gtag`,
+            options: {
+                trackingId: `G-P76W5VY5NZ`,
+                head: true,
+            },
+        },
+    ],
 }
