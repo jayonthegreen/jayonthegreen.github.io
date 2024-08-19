63aaaf46b256f80cc3309e63daad1b60a8282533
byjay <rururu0729@gmail.com>
2024-08-20 02:54:15 +0900

    Add canonical link for post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../component/Layout'
 import PostList from '../component/PostList'
+import Helmet from "react-helmet";
 
 class IndexPage extends React.Component {
-  render() {
-    return (
-      <Layout>  
-        <PostList
-          markdownNodes={this.props.data.allMarkdownRemark.edges.map(
-            ({ node }) => node
-          )}
-        />
-      </Layout>
-    )
-  }
+    render() {
+        return (
+            <Layout>
+                <Helmet>
+                    <link rel="canonical" href="https://jayonthegreen.github.io"/>
+                </Helmet>
+                <PostList
+                    markdownNodes={this.props.data.allMarkdownRemark.edges.map(
+                        ({node}) => node
+                    )}
+                />
+            </Layout>
+        )
+    }
 }
 
 export default IndexPage
