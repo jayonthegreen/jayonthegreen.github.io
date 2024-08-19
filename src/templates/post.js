963a215872009996ced65dfafa8496f9616bda67
byjay <rururu0729@gmail.com>
2024-08-19 14:32:58 +0900

    Remove facebook comment

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
-import { media } from '../utils/style'
+import {media} from '../utils/style'
 // import { BuyMe } from '../component/BuyMe';
-import { graphql } from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../component/Layout';
 
 
 const Wrapper = styled.div`
-    max-width: 600px;
-    padding: 1rem 0;
-    margin: auto;  
+  max-width: 600px;
+  padding: 1rem 0;
+  margin: auto;
 `
 
 const Time = styled.div`
@@ -26,7 +26,7 @@ const Title = styled.h1`
   font-weight: bold;
   font-size: 1.4rem;
   margin: 0.5rem;
-  text-align: center; 
+  text-align: center;
   word-break: keep-all;
   color: var(--title-color);
 `
@@ -40,18 +40,21 @@ const Description = styled.div`
 
 const Content = styled.div`
   margin-top: 2rem;
+
   & iframe {
     ${
-      media.mobile`
+            media.mobile`
         width: 100%;
         height: 100%;
       `
     }
   }
-  img{
-    display:block;
+
+  img {
+    display: block;
     margin: 0 auto;
   }
+
   h3 {
     line-height: 1.4;
   }
@@ -59,55 +62,44 @@ const Content = styled.div`
 
 class PostTemplate extends React.Component {
 
-  componentDidMount() {
-    this.facebookCommentInstall();
-  }
+    componentDidMount() {
+    }
 
-  facebookCommentInstall() {
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/ko_KR/sdk.js#xfbml=1&version=v3.1&appId=2072231716426066&autoLogAppEvents=1';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }
+    render() {
+        const post = this.props.data.markdownRemark
+        const {
+            title,
+            description,
+            keywords = [],
+            image = this.props.data.site.siteMetadata.image || '/img/og.jpeg',
+            date,
+        } = post.frontmatter;
+        const imageUrl = 'https://jayonthegreen.github.io' + image;
+        const meta = [
+            {name: 'title', content: title},
+            {name: 'description', content: description},
+            {name: 'keywords', content: [keywords || title].join(',')},
+            {name: 'image', content: imageUrl},
+            {property: 'og:description', content: description},
+            {property: 'og:title', content: title},
+            {property: 'og:image', content: imageUrl},
+        ]
 
-  render() {
-    const post = this.props.data.markdownRemark
-    const {
-      title,
-      description,
-      keywords = [],
-      image = this.props.data.site.siteMetadata.image || '/img/og.jpeg',
-      date,
-    } = post.frontmatter;
-    const imageUrl = 'https://jayonthegreen.github.io' + image;
-    const meta = [
-      {name: 'title', content: title},
-      {name: 'description', content: description},
-      {name: 'keywords', content: [keywords || title].join(',')},
-      {name: 'image', content: imageUrl},
-      {property: 'og:description', content: description},
-      {property: 'og:title', content: title},
-      {property: 'og:image', content: imageUrl},
-    ]
-
-    return (
-      <Layout>
-      <Wrapper>
-        <Helmet meta={meta}>
-          <title>{title}</title>
-        </Helmet>
-        <Time>{date}</Time>
-        <Title>{post.frontmatter.title}</Title>
-        <Description>{post.frontmatter.description}</Description>
-        <Content dangerouslySetInnerHTML={{ __html: post.html }} />
-        {/* <BuyMe/> */}
-      </Wrapper>
-      </Layout>
-    )
-  }
+        return (
+            <Layout>
+                <Wrapper>
+                    <Helmet meta={meta}>
+                        <title>{title}</title>
+                    </Helmet>
+                    <Time>{date}</Time>
+                    <Title>{post.frontmatter.title}</Title>
+                    <Description>{post.frontmatter.description}</Description>
+                    <Content dangerouslySetInnerHTML={{__html: post.html}}/>
+                    {/* <BuyMe/> */}
+                </Wrapper>
+            </Layout>
+        )
+    }
 }
 
 export default PostTemplate
