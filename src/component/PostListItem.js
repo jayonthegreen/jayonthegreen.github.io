bc76c909eb0fc511d9172bb930094eecfd8a5be0
byjay <rururu0729@gmail.com>
2024-09-02 21:42:04 +0900

    간격 조정

--- src/component/PostListItem.js
+++ src/component/PostListItem.js
@@ -3,47 +3,46 @@ import styled from 'styled-components'
 import hoverCss from './hoverCss'
 
 const Wrapper = styled.article`
-  margin-bottom: 1rem;
-  text-align: center;
-  padding: 1rem;
-  ${hoverCss}
+    margin-bottom: 0.6rem;
+    text-align: center;
+    padding: 0.5rem;
+    ${hoverCss}
 `
 
 const Meta = styled.div`
-  font-size: 0.8rem;
-  color: var(--subtitle-color);
+    font-size: 0.8rem;
+    color: var(--subtitle-color);
 `
 
 const Title = styled.h2`
-  font-size: 1.2rem;
-  line-height: 1.1;
-  margin: 0;
-  margin-bottom: 5px;
-  word-break: keep-all;
-  color: var(--title-color);
+    font-size: 1.2rem;
+    line-height: 1.1;
+    margin: 0 0 5px;
+    word-break: keep-all;
+    color: var(--title-color);
 `
 
 const Description = styled.div`
-  color: var(--subtitle-color);
-  font-size: 0.9rem;
-  line-height: 1.2rem;
-  word-break: keep-all;
+    color: var(--subtitle-color);
+    font-size: 0.9rem;
+    line-height: 1.1;
+    word-break: keep-all;
 `
 
 const maxLength = 100;
 
-const PostListItem = ({ title, date, description }) => (
-  <Wrapper>
-    <Meta>{date}</Meta>
-    <Title>{title}</Title>
-    {
-      description &&
-      <Description>
-        {description.slice(0, maxLength)}
-        {description.length > maxLength && '...'}
-      </Description>
-    }
-  </Wrapper>
+const PostListItem = ({title, date, description}) => (
+    <Wrapper>
+        <Meta>{date}</Meta>
+        <Title>{title}</Title>
+        {
+            description &&
+            <Description>
+                {description.slice(0, maxLength)}
+                {description.length > maxLength && '...'}
+            </Description>
+        }
+    </Wrapper>
 )
 
 export default PostListItem
