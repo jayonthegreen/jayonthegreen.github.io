0dcd0ddd9351ba9bd3b3e74f29f485f8e47001d8
byjay <rururu0729@gmail.com>
2024-10-27 12:39:06 +0900

    이미지 넣는건 생각보다 별로..

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,21 @@ import {graphql} from 'gatsby'
 import Layout from '../component/Layout'
 import PostList from '../component/PostList'
 import Helmet from "react-helmet";
+import styled from 'styled-components';
+
+const ImageContainer = styled.div`
+  width: 100%; /* 원하는 너비에 맞추세요 */
+  aspect-ratio: 16 / 6;
+  overflow: hidden;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+`;
+
 
 class IndexPage extends React.Component {
     render() {
@@ -11,6 +26,9 @@ class IndexPage extends React.Component {
                 <Helmet>
                     <link rel="canonical" href="https://jayonthegreen.github.io"/>
                 </Helmet>
+                {/*<ImageContainer>*/}
+                {/*    <StyledImage src="/img/CE126160-C3DD-48B2-964F-569530639FB9_1_201_a.jpeg" alt="sample image" />*/}
+                {/*</ImageContainer>*/}
                 <PostList
                     markdownNodes={this.props.data.allMarkdownRemark.edges.map(
                         ({node}) => node
