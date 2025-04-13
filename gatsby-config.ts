4216db3f1440eb2eb1700befba916e72aead1138
jay <kozha@kozhaui-MacBookAir-3.local>
2025-04-13 14:44:19 +0900

    Add site meta

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `jay`,
+    siteUrl: `https://jayonthegreen.github.io`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
